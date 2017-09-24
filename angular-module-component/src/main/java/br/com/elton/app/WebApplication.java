6d340955ab08b59bde0e01a5820e75a86ce8a466
Elton <Elton@192.168.25.6>
2017-09-24 12:33:06 -0300

    retirando configurações desnecessarias, implantando servico rest para renderizar pagina de index.

--- angular-module-component/src/main/java/br/com/elton/app/WebApplication.java
+++ angular-module-component/src/main/java/br/com/elton/app/WebApplication.java
@@ -2,21 +2,9 @@ package br.com.elton.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class WebApplication extends WebMvcConfigurerAdapter {
-
-	@Override
-	public void addViewControllers(final ViewControllerRegistry registry) {
-		registry.addViewController("/web").setViewName("forward:/views/index.html");
-	}
-
-	@Override
-	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-	}
+public class WebApplication {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(WebApplication.class, args);
